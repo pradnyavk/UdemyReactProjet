b2e75ac7e805b1f0803afd07e07d7e3dde57d222
Pradnya katigar <pradnyak@aaicp-dc.aai.cdac.in>
2022-10-11 15:55:13 +0530

    section 5

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 import './ExpenseForm.css'
 
-const ExpenseForm = () => {
+const ExpenseForm = (props) => {
     const [enteredTitle,setEnteredTitle] = useState('')
     const [enteredAmount,setEnteredAmount] = useState('')
     const [enteredDate,setEnteredDate] =useState('')
@@ -23,6 +23,8 @@ const ExpenseForm = () => {
             amount:enteredAmount,
             date:new Date(enteredDate)
         }
+
+        props.onSaveExpenseData(expenseData)
 
         setEnteredAmount('')
         setEnteredDate('')
--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -1,9 +1,19 @@
 import ExpenseForm from './ExpenseForm'
 import './NewExpense.css'
 
-const NewExpense = () => {
+const NewExpense = (props) => {
+
+    const saveExpenseHandler = (enteredExpenseDetails) => {
+        const expenseData = {
+            ...enteredExpenseDetails,
+            id:Math.random().toString()
+        }
+
+        props.onAddExpense(expenseData)
+
+}
     return (<div className="new-expense">
-        <ExpenseForm/>
+        <ExpenseForm onSaveExpenseData={saveExpenseHandler} />
     </div>
     )
 }
